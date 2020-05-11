0df052cec53c2dbad50ba92efa216461e861db24
高炼 <mangonob@qq.com>
2020-05-11 08:27:25 +0800

    [feat] Context

--- src/index.js
+++ src/index.js
@@ -2,78 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function FancyBorder(props) {
-  return <div className={"FancyBorder FancyBorder-" + props.color}>
-    {props.children}
-  </div>
-}
-
-function WelcomeDialog(props) {
-  return <FancyBorder color='blue'>
-    <h1 className="Dialog-title">
-      Welcome
-    </h1>
-    <p className="Dialog-message">
-      Thank you for visiting our spacecraft!
-    </p>
-  </FancyBorder>
-}
-
-function SplitPlane(props) {
-  return <div className="SplitPlane">
-    <div className="SplitPlane-left">
-      {props.left}
-    </div>
-    <div className="SplitPlane-right">
-      {props.right}
-    </div>
-  </div>
-}
-
-function Dialog(props) {
-  return <FancyBorder color="blue">
-    <h1 class-Name="Dialog-title">
-      {props.title}
-    </h1>
-    <p class-Name="Dialog-message">
-      {props.message}
-    </p>
-    {props.children}
-  </FancyBorder>
-}
-
-class SignUpDialog extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSignup = this.handleSignup.bind(this)
-    this.state = {login: null}
-  }
-
-  handleChange(event) {
-    this.setState({login: event.target.value})
-  }
-
-  handleSignup(event) {
-    alert(`Welcome aboard, ${this.state.login}!`)
-  }
-
-  render() {
-    return <Dialog title="Mars Exploration Program"
-      message="How should we refer to you">
-      <input value={this.state.login}
-        onChange={this.handleChange} />
-      <button onClick={this.handleSignup}>
-        Sign Me Up!
-      </button>
-    </Dialog>
-  }
-}
-
+const ThemeContext = React.createContext('light')
 class App extends React.Component {
   render() {
-    return <SignUpDialog />
+    return <ThemeContext.Provider value="dark">
+      <Toolbar />
+    </ThemeContext.Provider>
   }
+}
+
+function Toolbar(props) {
+  // Toolbar 组件接受一个额外的“theme”属性，然后传递给 ThemedButton 组件。
+  // 如果应用中每一个单独的按钮都需要知道 theme 的值，这会是件很麻烦的事，
+  // 因为必须将这个值层层传递所有组件。
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+}
+
+class ThemedButton extends React.Component {
+  static contextType = ThemeContext
+
+  render() {
+    return <ThemeContext.Consumer>
+      {
+        theme => <Button theme={theme} />
+      }
+    </ThemeContext.Consumer>
+  }
+}
+
+function Button(props) {
+  return <input type="button" value={props.theme}/>
 }
 
 // ========================================
